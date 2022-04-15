a405312a189d34e22976ade6006a5c025926f21a
Kehan Fu <kehanfu@gmail.com>
2022-04-15 00:12:50 +0000

    premerge changes

---
+++ public/scripts/app.js
@@ -0,0 +1,13 @@
+// $(document).ready(() => {
+
+//   window.onscroll = function() {scrollFunction()};
+
+//   function scrollFunction() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//       document.getElementById("header-outer").setAttribute('style', 'height: 30px');
+//     } else {
+//       document.getElementById("header-outer").setAttribute('style', 'height: 90px');
+//     }
+//   }
+
+// });
